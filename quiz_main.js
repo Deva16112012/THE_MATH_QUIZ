f5e31a210cdf5cfa024b6a18a77606a495357b9b
Deva16112012 <100427772+Deva16112012@users.noreply.github.com>
2022-06-25 18:42:55 +0530

    Add files via upload

--- quiz_main.js
+++ quiz_main.js
@@ -28,11 +28,11 @@ function check(){
     get_answer = document.getElementById("input_check_box").value;
     if(answer_turn == actual_answer){
         if(answer_turn == "player1"){
-                    update_player1_score +1;
+        update_player1_score = update_player1_score +1;
         document.getElementById("player1_score").innerHTML = update_player1_score;
         }
         else{
-            update_player2_score +1;
+            update_player2_score =update_player2_score +1;
             document.getElementById("player2_score").innerHTML = update_player2_score;    
         }
     }
